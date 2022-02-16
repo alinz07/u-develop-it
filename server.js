c1ce209e3b2a42a65b16dd2cd021273a8fe9de19
Tony Linz <tonyllinz@gmail.com>
2022-02-15 19:42:00 -0600

    add query for all rows in the candidates table

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username,
+        user: 'root',
+        //your mysql password
+        password: 'Caradhras100!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World'
-//     });
-// });
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //default response for any other request (Not Found)
 //has to be the last one otherwise it will override all others
